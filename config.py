c68512841d2dbf7c0ae11a2cab18b807dcf53cc7
vinnyotach7 <vinnyotach7@gmail.com>
2018-11-18 22:59:24 +0300

    this is where i keep my secret key and api key

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+
+
+class Config:
+
+    SECRET_KEY = os.environ.get('00198a1a68706d942f5e204fe165696f')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vicklyne:Moringa123@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vicklyne:Moringa123@localhost/pitch'
+    DEBUG = True    
+
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass    
+
+
+config_options={
+    'development':DevConfig,
+    'production': ProdConfig
+}
