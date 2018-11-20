9e55dad6ceb441f811c1a3b460212561e53c0f0d
vinnyotach7 <vinnyotach7@gmail.com>
2018-11-20 05:43:07 +0300

    added some missing code to the file

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,18 +4,12 @@ from wtforms.validators import Required
 
 
 class PitchForm(FlaskForm):
-
     post = StringField('Your name',validators=[Required()])
-
     body = TextAreaField('Pitch')
-
-    category = SelectField('Pick Category',
-                          choices=[('PRODUCT', 'PRODUCT'),
-                                ('INTERVIEW', 'INTERVIEW'),
-                                ('PICK-UP', 'PICK-UP'),
+    category_id = SelectField('Pick Category',choices=[('PRODUCT', 'PRODUCT'),('INTERVIEW', 'INTERVIEW'),('PICK-UP', 'PICK-UP'),
                                    ('PROMOTION', 'PROMOTION')],
                           validators=[Required()])
-   
+    content = StringField('content', validators=[Required()])
     submit = SubmitField('Pitch')
 
 class CommentForm(FlaskForm):
@@ -27,5 +21,5 @@ class CommentForm(FlaskForm):
     submit = SubmitField('Comment')
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about yourself.',validators = [Required()])
     submit = SubmitField('Submit')
